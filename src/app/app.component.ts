08b8a4a14e6456b038e43156cea35cdf50aad6ba
Taylor Buckner <tbuckner@theappraisallane.com>
2018-10-24 16:53:31 -0500

    Initial POC for Angular6 Electron app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { map, takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'electron-angular';
+  public max = 1;
+  public current = 0;
+
+  start() {
+    const interval$ = interval(100)
+      .pipe(
+        takeWhile(_ => !this.isFinished),
+        tap(i => this.current += 0.1)
+      );
+    // Unsubcribe later on.
+    interval$.subscribe();
+  }
+
+  finish() {
+    this.current = this.max;
+  }
+
+  reset() {
+    this.current = 0;
+  }
+
+  // Ensures Max value is never less than 0.1;
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  // Ensures current value is never less than zero.
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  // Returns truthy if current value is above or equal to max value.
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
 }
